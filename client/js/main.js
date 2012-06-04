dec719fa412784adb286fc4d47386ef47cb9a0c8
Sebastian Springer <sebastian.springer@mayflower.de>
2012-06-04 17:42:37 +0200

    Introduced some require2 features to main.js

--- client/js/main.js
+++ client/js/main.js
@@ -1,10 +1,21 @@
+/*global requirejs */
+
 requirejs.config({
     baseUrl: 'js/lib',
     paths: {
         app: '../app'
+    },
+    shim: {
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        }
     }
 });
 
-requirejs(['jquery'], function ($) {
-    // put main code in here
+requirejs(['backbone', 'app/router'], function (Backbone, Router) {
+    "use strict";
+
+    var router = new Router();
+    Backbone.history.start();
 });
