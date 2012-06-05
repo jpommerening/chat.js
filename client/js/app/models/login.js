55852a4386d764c7f90543f93c0a8e4115044440
Sebastian Springer <sebastian.springer@mayflower.de>
2012-06-05 11:52:25 +0200

    Added login architecture
    model, view and template

---
+++ client/js/app/models/login.js
@@ -0,0 +1,18 @@
+/**
+ * Login Model, represents the business logic needed to log in a certain user
+ *
+ * @fileOverview models/login.js
+ * @author Sebastian Springer
+ * @class models_login
+ */
+
+/*global define, Backbone */
+
+
+define(function () {
+    "use strict";
+
+    return Backbone.Model.extend({
+
+    });
+});
---
+++ client/js/app/views/login.js
@@ -0,0 +1,27 @@
+/**
+ * Login View, visualizes the login process
+ *
+ * @fileOverview views/login.js
+ * @author Sebastian Springer
+ * @class views_login
+ */
+
+/*global define, Backbone */
+
+define(['text!app/views/login.html'], function (template) {
+    "use strict";
+
+    return Backbone.View.extend({
+
+        className: 'views_login',
+        tagName: 'div',
+
+        events: {},
+
+        initialize: function () {
+        },
+
+        render: function () {
+        }
+    });
+});
