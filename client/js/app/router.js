d8f9725e2efe04cf6ead36077b2be5d67c331beb
Sebastian Springer <sebastian.springer@mayflower.de>
2012-06-06 21:10:12 +0200

    Open socket connection in the router

--- client/js/app/router.js
+++ client/js/app/router.js
@@ -1,11 +1,29 @@
-/*global define */
+/*global define, Backbone, socket, io */
+
+
+var socket = null;
+
 define(['backbone', 'app/controllers/login'], function (Backbone, LoginController) {
     "use strict";
 
+
+
     return Backbone.Router.extend({
+
+        url: 'chat.basti.dev',
+        port: '1337',
+
 
         initialize: function () {
             Backbone.history.start();
+
+            this._openSocket();
+        },
+
+        _openSocket: function () {
+            if (null === socket) {
+                socket = io.connect(this.url + ':' + this.port);
+            }
         },
 
         routes: {
