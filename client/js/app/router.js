9014a139da01268c7e2a0f4408b079fc3391e687
Thorsten Rinne <thorsten@rinne.info>
2012-09-18 20:26:16 +0200

    Fixed location

--- client/js/app/router.js
+++ client/js/app/router.js
@@ -5,7 +5,7 @@ define(['backbone', 'socket.io', 'app/controllers/chat'], function (Backbone, io
 
     return Backbone.Router.extend({
 
-        url: document.URL,
+        url: location.host,
         port: '1338',
 
         initialize: function () {
